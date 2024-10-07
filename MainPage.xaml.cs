196efe028e810363d162167cafa1c94b18dbf913
Shintaro Abe <dictav@gmail.com>
2024-10-07 22:13:54 +0900

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MyMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		LoadWebViewAsync();
+	}
+
+	private async Task LoadWebViewAsync()
+	{
+		var httpClient = new HttpClient();
+
+		// set token
+		httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer YOUR_TOKEN");
+
+		var response = await httpClient.GetStringAsync("URL");
+		
+		WebView.Source = new HtmlWebViewSource
+		{
+			Html = response
+		};
+	}
+}
